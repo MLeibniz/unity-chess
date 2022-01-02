235255ec136059da9ebd4ab113b8baf3db900fa5
Marcos Leibniz <marcos.leibniz@gmail.com>
2022-01-02 06:05:57 -0300

    seletor adicionado

--- Chess.cs
+++ Chess.cs
@@ -1,8 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System;
 
 public static class Chess
 {
     // TODO: inicia a posição, administra a vez de cada jogador e checa checkmates e repetições
+    public delegate void SelectorHandler(object sender, EventArgs e);
+    public static event SelectorHandler OnSelection;
+
+    public static void TriggerSelector()
+    {
+        if(OnSelection != null)
+        {
+            OnSelection(null,null);
+        }
+    }
 }
--- Square.cs
+++ Square.cs
@@ -1,54 +1,73 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Square : MonoBehaviour
 {
-    [SerializeField] Board board;
-    SpriteRenderer sr;
-    Color mainColor, selectedColor;
-    int x;
-    int y;
     bool blackSquare;
 
     void Start()
     {
         Cache();
-        ConfigColor();
+        SetUpColor();
     }
 
+    SpriteRenderer sr;
+    int x, y;
     void Cache()
     {
         sr = GetComponent<SpriteRenderer>();
-    }
-
-    void ConfigColor()
-    {
         x = (int)GetComponent<Transform>().position.x;
         y = (int)GetComponent<Transform>().position.y;
+    }
+
+    Color mainColor, selectedColor;
+    void SetUpColor()
+    {
         blackSquare = (x + y) % 2 == 0 ? true : false;
         mainColor = blackSquare? ChessConfig.BlackSquareColor : ChessConfig.WhiteSquareColor;
         selectedColor = blackSquare ? ChessConfig.SelectedBlackSquareColor : ChessConfig.SelectedWhiteSquareColor;
-        ResetColor();
+        ChangeColor(mainColor);
     }
 
-    private void OnMouseDown() 
+    bool selected = false, deselecting = false, legalmove = false;    
+    void OnMouseDown() 
     {
-        ChangeColor(selectedColor);
+        // condição teste
+        if(!legalmove)
+        {
+            deselecting = selected? true : false;
+            Debug.Log("Quadrado clicado com selected: " + selected);
+            Chess.TriggerSelector();
+            Chess.OnSelection += Deselect;
+            selected = true;
+            ChangeColor(selectedColor);
+        }
+        else
+        {
+            //TODO método para mover a peça,
+        }
     }
 
-    private void OnMouseUp() 
+    void OnMouseUpAsButton() 
     {
-        ResetColor();    
+        if(deselecting)
+        {
+            Debug.Log("Square is being deselectd");
+            Chess.TriggerSelector();
+            selected = false;
+            Chess.OnSelection -= Deselect; 
+        }
+    }
+
+    void Deselect(object sender, EventArgs e)
+    {
+        selected = false;
+        ChangeColor(mainColor);
+        Chess.OnSelection -= Deselect;
     }
 
     void ChangeColor(Color color)
     {
         sr.color = color;
-    }
-
-    void ResetColor()
-    {
-        sr.color = mainColor;
     }
 }
