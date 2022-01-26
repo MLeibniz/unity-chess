fb0eba3c78f6540832b15fec156eba64830aacd8
M. Leibniz <marcos.leibniz@gmail.com>
2022-01-25 23:49:01 -0300

    King fix

--- King.cs
+++ King.cs
@@ -4,19 +4,19 @@ public class King : Piece
 {
     public override void SetMaxMoves()
     {
-        moves = new Move[9];
+        moves = new Square[9];
     } 
         
     public override void GetMoves()
     {
-        moves[4] = new Move(-1,-1);
+        moves[4] = new Square(-1,-1);
         int count = 0;
         for (int r = -1; r < 2; r++)
         {
             for (int f = -1; f < 2; f++)
             {
                 if(count == 4){count++ ; continue;}
-                moves[count] = new Move(file + f, rank + r);
+                moves[count] = board.squares[file + f, rank + r].GetComponent<Square>();
                 count++;
             }       
         }
