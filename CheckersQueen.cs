bc66ec9cc86016d2879d340f8c2192adfff91c74
M. Leibniz <marcos.leibniz@gmail.com>
2022-01-26 00:07:30 -0300

    Checkers Queen Move Removed

--- CheckersQueen.cs
+++ CheckersQueen.cs
@@ -2,28 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CheckersPiece : Piece
+public class CheckersQueen : Piece
 {
-    public override Vector2[] GetLegalMoves(Vector2 curPos, bool isBlack)
+    public override void GetMoves()
     {
-        int colorFactor = isBlack? 1 : -1;
-        Vector2[] legal = new Vector2[4];
-        legal[0] = new Vector2 (curPos.x + 1, curPos.y + 1 * colorFactor);
-        legal[1] = new Vector2 (curPos.x + 2, curPos.y + 2 * colorFactor);
-        legal[2] = new Vector2 (curPos.x - 1, curPos.y + 1 * colorFactor);
-        legal[3] = new Vector2 (curPos.x - 2, curPos.y + 2 * colorFactor);
-
-        for (int i = 0; i < 4; i++)
-        {
-            int x = (int) legal[i].x;
-            int y = (int) legal[i].y;
-            if(x < 0 || x > 7 || y < 0 || y > 7)
-            {
-                legal[i] = new Vector2(-1,-1);
-            }
-        }
-        return legal;
+        /*
+        int colorFactor = bottomPlayer? 1 : -1;
+        Square[] legal = new Square[4];
+        legal[0] = board.squares[file + 1, rank + 1 * colorFactor];
+        legal[1] = board.squares[file + 2, rank + 2 * colorFactor];
+        legal[2] = board.squares[file - 1, rank + 1 * colorFactor];
+        legal[3] = board.squares[file - 2, rank + 2 * colorFactor];
+        */
     }
 
-    // Start is called before the first frame update
+    public override void SetMaxMoves()
+    {
+        // set array for moves
+    }
 }
